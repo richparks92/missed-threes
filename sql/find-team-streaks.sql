8850d3c5b85cd8f3be464c7ba6bc39a518981798
richparks92 <richparks92@gmail.com>
2024-02-21 08:48:09 -0800

    more sql updates

--- sql/find-team-streaks.sql
+++ sql/find-team-streaks.sql
@@ -1,11 +1,40 @@
-WITH lags AS (
-		SELECT 
-			*, 
-			LAG(is_make) 
-				OVER(
-					ORDER BY team_id, game_date, event_num) 
-			AS prev 
-		FROM shots_sample
+WITH  team_shots_raw AS (
+	SELECT 
+		pbp2.GAME_ID AS game_id, 
+		lgl.GAME_DATE AS game_date, 
+		EVENTNUM as event_num,  
+		PLAYER1_ID AS player_id,  
+		PLAYER1_TEAM_ID  AS team_id,
+		CASE
+			WHEN COALESCE(HOMEDESCRIPTION, '') <> '' THEN HOMEDESCRIPTION
+			ELSE VISITORDESCRIPTION
+		END AS description
+	FROM  PLAY_BY_PLAYS_STATS_2 pbp2
+	LEFT JOIN LEAGUE_GAME_LOGS lgl ON pbp2.GAME_ID = lgl.GAME_ID
+	WHERE lgl.WL = "W" AND DESCRIPTION LIKE "%3PT%"
+	ORDER BY pbp2.PLAYER1_TEAM_ID, GAME_DATE, pbp2.GAME_ID, EVENTNUM
+	--LIMIT 10000
+		)
+,team_shots AS (
+	SELECT 
+		game_id,
+		game_date,
+		event_num,
+		player_id,
+		team_id,
+		CASE WHEN description LIKE "MISS%" THEN False ELSE True END AS  is_make
+	FROM
+		team_shots_raw
+)
+
+,lags AS (
+	SELECT 
+		*, 
+		LAG(is_make) 
+			OVER(
+				ORDER BY team_id, game_date, event_num) 
+		AS prev 
+	FROM team_shots
 		)
 
 , streaks AS (
